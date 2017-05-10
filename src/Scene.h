b13233252460102948e512009084f923ffa3fb61
Lucas <lucas.r.frank@gmail.com>
2017-05-10 11:22:29 -0300

    Começando iluminação

--- src/Scene.h
+++ src/Scene.h
@@ -5,16 +5,21 @@
 class Scene{
     private:
         GLMmodel* pmodel[11];
+        GLfloat light0_position [];
         void drawTable();
         void drawTeapot();
         void drawTorus();
         void drawFlower();
         void drawChairs();
         void drawPlates();
+        void drawRose();
+        void setMaterial(GLfloat *ambiente, GLfloat *difusa, GLfloat *especular,GLfloat *brilho);
     public:
         Scene();
         ~Scene();
         void drawScene();
+        void setLightPosition(float x,float y,float z);
+        float getLightPosition(int i);
 
 };
 
